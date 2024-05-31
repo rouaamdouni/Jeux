4a32db9126e8489219861e6209a6f75bc214ceab
Ahmed Gafsi <ahmedgafsi-@outlook.com>
2024-05-31 18:13:19 +0100

    Safe Check Point number 3 : training Popups done

--- app.js
+++ app.js
@@ -2,14 +2,35 @@ import { Game } from './Entities/Game.js'
 
 const trainBlueWarriorsBtn = document.getElementById('trainBlueWarriors');
 const trainBlueWarriorsPopup = document.querySelector('.blueWarriors');
+const trainRedWarriorsBtn = document.getElementById('trainRedWarriors');
+const trainRedWarriorsPopup = document.querySelector('.redWarriors');
 
-trainBlueWarriorsBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    if (trainBlueWarriorsPopup.style.display === 'none') {
-        trainBlueWarriorsPopup.style.display = 'flex';
+
+function openPopup(element){
+    if (element.style.display === 'none') {
+        element.style.display = 'flex';
     }
     else {
-        trainBlueWarriorsPopup.style.display = 'none';
+        element.style.display = 'none';
+    }
+}
+
+function closePopup(element){
+    element.style.display = 'none';
+}
+
+trainBlueWarriorsBtn.addEventListener('click', (event) => {
+    if(!event.target.matches('.trainBlueWarriors')){
+        openPopup(trainBlueWarriorsPopup);
+        closePopup(trainRedWarriorsPopup);
+    }
+
+});
+
+trainRedWarriorsBtn.addEventListener('click', (event) => {
+    if(!event.target.matches('.trainRedWarriors')){
+        openPopup(trainRedWarriorsPopup);
+        closePopup(trainBlueWarriorsPopup);
     }
 });
 
